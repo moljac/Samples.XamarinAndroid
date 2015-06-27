55e2ed43bfafeb16ffcd920934cc8a2b95d588d5
moljac <mcvjetko@holisticware.net>
2015-06-27 22:57:44 +0200

    service added

---
+++ tutorial-samples/85-Kiosk/Kiosk.XamarinAndroid/KioskService.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+
+
+namespace Kiosk.Sample.XamarinAndroid
+{
+	/// <summary>
+	/// Kiosk service.
+	///		Disable the home button and detect when new applications are opened
+	///		
+	///
+	///		Since Android 4 there is no effective method to deactivate the home button. 
+	///		That is the reason why we need another little hack. In general the idea is to 
+	///		detect when a new application is in foreground and restart your activity 
+	///		immediately.
+	/// </summary>
+	[Android.App.Service]
+	public class KioskService : Android.App.Service
+	{
+
+		private static long INTERVAL = Java.Util.Concurrent.TimeUnit.Seconds.ToMillis(2); 
+		// periodic interval to check in seconds -> 2 seconds
+		private static string TAG = typeof(KioskService).Name;
+		private static string PREF_KIOSK_MODE = "pref_kiosk_mode";
+
+		private Java.Lang.Thread t = null;
+		private Android.Content.Context ctx = null;
+		private bool running = false;
+
+
+		public override void OnDestroy() 
+		{
+			Android.Util.Log.Info(TAG, "Stopping service 'KioskService'");
+			running =false;
+			base.OnDestroy();
+
+			return;
+		}
+
+		public override Android.App.StartCommandResult  OnStartCommand
+															(
+																Android.Content.Intent intent, 
+																Android.App.StartCommandFlags flags, 
+																int startId
+															) 
+		{
+			Android.Util.Log.Info(TAG, "Starting service 'KioskService'");
+			running = true;
+			ctx = this;
+
+			// start a thread that periodically checks if your app is in the foreground
+			t = new Java.Lang.Thread
+					(
+						() => 
+						{
+							do 
+							{
+								HandleKioskMode();
+								try
+								{
+									Java.Lang.Thread.Sleep(INTERVAL);
+								} 
+								catch (Java.Lang.InterruptedException e) 
+								{
+									Android.Util.Log.Info(TAG, "Thread interrupted: 'KioskService'");
+								}
+							} while(running);
+							StopSelf();
+						}
+					);
+
+			t.Start();
+			return Android.App.StartCommandResult.NotSticky;
+		}
+
+		private void HandleKioskMode() 
+		{
+			// is Kiosk Mode active? 
+			if(IsKioskModeActive()) 
+			{
+				// is App in background?
+				if(IsInBackground()) 
+				{
+					RestoreApp(); // restore!
+				}
+			}
+
+			return;
+		}
+
+		private bool IsInBackground() 
+		{
+			Android.App.ActivityManager am = 
+					(Android.App.ActivityManager) 
+						ctx.GetSystemService(Android.Content.Context.ActivityService);
+
+		List<Android.App.ActivityManager.RunningTaskInfo> taskInfo = null;
+
+		taskInfo = am.GetRunningTasks(1);
+		Android.Content.ComponentName componentInfo = null;
+		componentInfo =  taskInfo.Get(0).TopActivity;
+		return (!ctx.getApplicationContext().getPackageName().equals(componentInfo.getPackageName()));
+		}
+
+		private void RestoreApp() 
+		{
+			// Restart activity
+			Android.Content.Intent i = new Android.Content.Intent(ctx, typeof(MyActivity));
+			i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			ctx.startActivity(i);
+		}
+
+		public bool IsKioskModeActive
+						(
+						//Android.Content.Context context
+						)
+		{
+			SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+			return sp.getBoolean(PREF_KIOSK_MODE, false);
+		}
+
+		public override Android.OS.IBinder OnBind(Android.Content.Intent intent) 
+		{
+			return null;
+		}
+
+
+	}
+}
