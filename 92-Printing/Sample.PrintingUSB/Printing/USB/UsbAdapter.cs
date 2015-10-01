a82051d47ee87143264b826d27a012d166f433e7
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2015-10-01 23:50:13 +0200

    test jars

---
+++ 92-Printing/Sample.PrintingUSB/Printing/USB/UsbAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.PrintingUSB
+{
+	public class UsbAdapter
+	{
+		public UsbAdapter ()
+		{
+		}
+	}
+}
+
---
+++ productivity/tools/JarSniffer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JarSniffer
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
