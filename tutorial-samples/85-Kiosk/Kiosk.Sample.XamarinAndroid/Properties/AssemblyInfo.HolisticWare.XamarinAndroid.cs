04f8933d144620f9494bd8a92dc02f7483a4b7d8
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2015-10-10 23:31:03 +0200

    cleanup

--- tutorial-samples/85-Kiosk/Kiosk.Sample.XamarinAndroid/Properties/AssemblyInfo.HolisticWare.XamarinAndroid.cs
+++
@@ -1,6 +0,0 @@
-﻿// HolisticWare Xamarin.Android assembly wide settings
-// modifying AndroidManifest.xml
-
-using Android.App;
-
-[assembly: UsesPermission(Name = "android.permission.RECEIVE_BOOT_COMPLETED")]
