9492e6038ffdd3daee9cd27688598cc4a0dcec19
moljac <mcvjetko@holisticware.net>
2015-11-16 14:11:23 +0100

    toolbar sample app compat

---
+++ tutorial-samples/45-Toolbar/Toolbar-API-v4x-AppCompatSupport/SupportToolbar/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace SupportToolbar
+{
+	[Activity( MainLauncher = true, Theme="@style/MyTheme")]
+	public class MainActivity :
+	 //AppCompatActivity
+	 ActionBarActivity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar> (Resource.Id.toolbar);
+
+			SetSupportActionBar (toolbar);
+		}
+
+
+		public override bool OnCreateOptionsMenu (IMenu menu)
+		{
+			MenuInflater.Inflate (Resource.Menu.menu, menu);
+			return base.OnCreateOptionsMenu (menu);
+		}
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{	
+			Toast.MakeText(this, "Top ActionBar pressed: " + item.TitleFormatted, ToastLength.Short).Show();
+			return base.OnOptionsItemSelected (item);
+		}
+
+	}
+}
+
+
