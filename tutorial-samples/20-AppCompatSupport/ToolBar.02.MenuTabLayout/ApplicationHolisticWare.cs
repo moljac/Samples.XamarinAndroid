f2fc5f6dd0edcfe8f1321cad5c08a4b18ca83959
moljac <mcvjetko@holisticware.net>
2015-11-26 12:46:02 +0100

    toolbar 2nd version

---
+++ tutorial-samples/20-AppCompatSupport/ToolBar.02.MenuTabLayout/ApplicationHolisticWare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Runtime;
+
+namespace HolisticWare.ToolbarSimple
+{
+	[Application
+		(
+			Theme="@style/AppThemeHolisticWare"	// descendant of the Theme.AppCompat
+		)
+	]
+	public class ApplicationHolisticWare : Application
+	{
+		public ApplicationHolisticWare ()
+		{
+		}
+
+		public ApplicationHolisticWare(IntPtr javaReference, JniHandleOwnership transfer) 
+			: base(javaReference, transfer)
+	    {
+	    }
+
+	}
+}
+
