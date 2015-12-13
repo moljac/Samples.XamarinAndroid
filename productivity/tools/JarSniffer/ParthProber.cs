1b5c9cf69d2d4742049986c9818e82fb14898c06
moljac <mcvjetko@holisticware.net>
2015-12-14 00:06:15 +0100

    PathProber

---
+++ productivity/tools/JarSniffer/ParthProber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JarSniffer
+{
+    public class ParthProber
+    {
+        Dictionary<string, string[]> folders_to_probe = null;
+
+        string[] path_xamarin_android_binding_project_folder_jars = 
+                                    new string[] 
+                                        {
+                                            "..",
+                                            "jars"
+                                        };
+
+            string[] path_project_data = 
+                                    new string[] 
+                                        {
+                                            "..",
+                                            "..",
+                                            "..",
+                                            "jars"
+                                        };
+
+
+        public ParthProber()
+        {
+            folders_to_probe = new Dictionary<string, string[]>();
+
+            folders_to_probe.Add
+                    (
+                        "jars in project",
+                        new string[] { "..", "jars"}
+                    );
+            folders_to_probe.Add
+                    (
+                        "jars during tests in project",
+                        new string[] { "..", "..", "..", "jars"}
+                    );
+        }
+    }
+}
+
