74e1a4ed7ac6235b3c05d3c978b0dff2e1e9f341
moljac <mcvjetko@holisticware.net>
2016-03-01 18:39:02 +0100

    ApiXml class

---
+++ productivity/tools/Libraries/HolisticWare.Productivity.XamarinAndroid.JavaUtilities/AOSP/ApiXml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HolisticWare.Productivity.XamarinAndroid.JavaUtilities
+{
+	public partial class ApiXml
+	{
+		public static string FormatXml(string inputXml)
+		{
+			XmlDocument document = new XmlDocument();
+			document.Load(new StringReader(inputXml));
+
+			StringBuilder builder = new StringBuilder();
+			using (XmlTextWriter writer = new XmlTextWriter(new StringWriter(builder)))
+			{
+				writer.Formatting = Formatting.Indented;
+				document.Save(writer);
+			}
+
+			return builder.ToString();
+		}
+			
+		public static Dictionary<string, string> ReplacementsMappings = new Dictionary<string, string> () { {
+				"metadata",
+				@"
+				<?xml version=""1.0"" encoding=""UTF-8"" ?>
+				<metadata>
+					<!-- PLACEHOLDER_API -->
+				</metadata>
+				"
+			}, {
+				"metadata api",
+				@"
+				<?xml version=""1.0"" encoding=""UTF-8"" ?>
+				<metadata>
+					<api>
+						<!-- PLACEHOLDER_PACKAGE -->
+					</api>
+				</metadata>
+				"
+			},
+					
+		};
+	}
+}
+
