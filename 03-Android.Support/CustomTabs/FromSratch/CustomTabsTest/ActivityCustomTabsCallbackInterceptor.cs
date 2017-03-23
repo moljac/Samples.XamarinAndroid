cb16c63b45f69f3f6b7a4b30f53e69963e6aafde
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-03-23 10:00:43 +0100

    IntentFilter tweaking

--- 03-Android.Support/CustomTabs/FromSratch/CustomTabsTest/ActivityCustomTabsCallbackInterceptor.cs
+++ 03-Android.Support/CustomTabs/FromSratch/CustomTabsTest/ActivityCustomTabsCallbackInterceptor.cs
@@ -17,11 +17,6 @@ namespace CustomTabsTest
     /// Activity custom tabs callback interceptor.
     /// https://developer.chrome.com/multidevice/android/customtabs
     /// </summary>
-    /// <see cref="https://developer.android.com/training/app-links/index.html"/> 
-    /// <see cref="http://stackoverflow.com/questions/24236701/how-to-receive-urls-with-xamarin-intent-filters"/>
-    /// <see cref="http://stackoverflow.com/questions/42470000/cant-open-youtube-in-custom-tabs#comment72171802_42470934"/>
-    /// <see cref="https://github.com/KevinDockx/IdentityModel.OidcClient.Samples/blob/master/AndroidClientChromeCustomTabs/AndroidClientChromeCustomTabs/CallbackInterceptorActivity.cs"/>
-    /// 
     [
         Activity
         (
@@ -37,19 +32,21 @@ namespace CustomTabsTest
                     Intent.CategoryDefault,
                     Intent.CategoryBrowsable
                 },
+			DataScheme = "xamarin-auth",
+			DataHost = "localhost",
+            /*
             DataSchemes = new[] 
             { 
                 //"http", "https" 
                 "xamarin-auth"
             },
-            // DataScheme = "https"
             DataHosts = new[] 
             { 
                 //"*.xamarin.com", "xamarin.com", 
                 "localhost", 
                 //"127.0.0.1" 
             },
-            // DataHost = "www.xamarin.com",
+            */
             DataMimeType = "text/plain"
         )
     ]
